8d53ef8280532d519347137707aa7537a00b9db7
Rodrigo <mitrerod@gmail.com>
2025-12-09 19:22:21 -0300

    Se crea template publicaciones.php

---
+++ templates/publicaciones.php
@@ -0,0 +1,47 @@
+<section class="py-5 bg-body">
+	<h2 class="fs-2 fw-bold text-center mb-5">Publicaciones</h2>
+	<div class="container">
+		<div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 pt-3">
+			<!-- Publicación 1 -->
+			<div class="col">
+				<div class="card h-100 shadow-sm border-0 rounded-3 bg-white">
+          <img src="<?= SERVER_URI ?>images/1-noticias.jpg" class="card-img-top" alt="Noticia Principal">
+          <div class="card-body d-flex flex-column justify-content-between p-4">
+						<h3 class="card-title fw-bold mb-3 fs-6">Lorem ipsum dolor sit amet consectetur. Sollicitudin viverra.</h3>
+						<a href="#" class="btn btn-sm btn-outline-primary mt-3">Conoce más</a>
+          </div>
+        </div>
+			</div>
+			<!-- Publicación 1 -->
+			<div class="col">
+				<div class="card h-100 shadow-sm border-0 rounded-3 bg-white">
+          <img src="<?= SERVER_URI ?>images/1-noticias.jpg" class="card-img-top" alt="Noticia Principal">
+          <div class="card-body d-flex flex-column justify-content-between p-4">
+						<h3 class="card-title fw-bold mb-3 fs-6">Lorem ipsum dolor sit amet consectetur. Sollicitudin viverra.</h3>
+						<a href="#" class="btn btn-sm btn-outline-primary mt-3">Conoce más</a>
+          </div>
+        </div>
+			</div>
+			<!-- Publicación 1 -->
+			<div class="col">
+				<div class="card h-100 shadow-sm border-0 rounded-3 bg-white">
+          <img src="<?= SERVER_URI ?>images/1-noticias.jpg" class="card-img-top" alt="Noticia Principal">
+          <div class="card-body d-flex flex-column justify-content-between p-4">
+						<h3 class="card-title fw-bold mb-3 fs-6">Lorem ipsum dolor sit amet consectetur. Sollicitudin viverra.</h3>
+						<a href="#" class="btn btn-sm btn-outline-primary mt-3">Conoce más</a>
+          </div>
+        </div>
+			</div>
+			<!-- Publicación 1 -->
+			<div class="col">
+				<div class="card h-100 shadow-sm border-0 rounded-3 bg-white">
+          <img src="<?= SERVER_URI ?>images/1-noticias.jpg" class="card-img-top" alt="Noticia Principal">
+          <div class="card-body d-flex flex-column justify-content-between p-4">
+						<h3 class="card-title fw-bold mb-3 fs-6">Lorem ipsum dolor sit amet consectetur. Sollicitudin viverra.</h3>
+						<a href="#" class="btn btn-sm btn-outline-primary mt-3">Conoce más</a>
+          </div>
+        </div>
+			</div>
+		</div>
+	</div>
+</section>
