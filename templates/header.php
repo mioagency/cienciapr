90b6668fcf4aa5cbf9f5e45ff929e2c7fae1038a
Rodrigo <mitrerod@gmail.com>
2025-12-10 20:05:16 -0300

    Actualización por Gabriel

--- templates/header.php
+++ templates/header.php
@@ -19,6 +19,7 @@ define('ACTIVE_PAGE', basename($_SERVER['PHP_SELF'], ".php"));
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="robots" content="noindex, nofollow" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" xintegrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link href="<?= SERVER_URI ?>css/main.css" rel="stylesheet">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
