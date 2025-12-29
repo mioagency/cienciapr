d2fec777b128aec2f2173a2236de8f57478346e1
Rodrigo <mitrerod@gmail.com>
2025-12-29 20:17:57 -0300

    Aplicado degradado en Footer BG

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,4 @@
-    <footer id="footer" class="bg-dark text-light pt-5 pb-4">
+    <footer id="footer" class="bg-dark text-light pt-5 pb-4 bg-footer-gradient">
       <div class="container">
         <div class="row">
           <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
