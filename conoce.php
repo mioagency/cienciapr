813dde96968940200f1669dce4a8ea70d3f8cb99
Rodrigo <mitrerod@gmail.com>
2026-01-24 19:06:19 -0300

    Reemplazo de código por template patrocinadores

--- conoce.php
+++ conoce.php
@@ -463,70 +463,8 @@ include_once('templates/breadcrumbs.php');
 		</div>
 	</section>
 
-	<!-- SECCIÓN: PATROCINADORES -->
-	<section id="nuestros-patrocinadores" class="container-fluid py-5">
-		<div class="row d-flex justify-content-center mb-5">
-			<div class="col-12 text-center">
-				<h2 class="fs-2 fw-bold text-body mb-3">Patrocinadores</h2>
-				<p class="lead text-muted col-lg-8 mx-auto">CienciaPR trabaja de la mano con docenas de organizaciones locales e internacionales que apoyan su misión mediante alianzas, programas educativos, fondos y recursos.</p>
-			</div>
-		</div>
-		
-		<!-- Contenedor para Scroll Horizontal -->
-		<div class="container-fluid p-0">
-			<!-- CLASE RENOMBRADA: 'carousel-scrollable' -->
-			<div class="row g-4 flex-nowrap overflow-x-auto pb-3 align-items-center carousel-scrollable">
-				<!-- Logo 1 -->
-				<div class="col-auto flex-shrink-0 text-center" style="width: 200px;">
-					<img src="https://placehold.co/150x60/f8f9fa/75848F?text=Company+1" class="img-fluid opacity-75" alt="Logo Patrocinador 1">
-				</div>
-				<!-- Logo 2 -->
-				<div class="col-auto flex-shrink-0 text-center" style="width: 200px;">
-					<img src="https://placehold.co/150x60/f8f9fa/75848F?text=Company+2" class="img-fluid opacity-75" alt="Logo Patrocinador 2">
-				</div>
-				<!-- Logo 3 -->
-				<div class="col-auto flex-shrink-0 text-center" style="width: 200px;">
-					<img src="https://placehold.co/150x60/f8f9fa/75848F?text=Company+3" class="img-fluid opacity-75" alt="Logo Patrocinador 3">
-				</div>
-				<!-- Logo 4 -->
-				<div class="col-auto flex-shrink-0 text-center" style="width: 200px;">
-					<img src="https://placehold.co/150x60/f8f9fa/75848F?text=Company+4" class="img-fluid opacity-75" alt="Logo Patrocinador 4">
-				</div>
-				<!-- Logo 5 -->
-				<div class="col-auto flex-shrink-0 text-center" style="width: 200px;">
-					<img src="https://placehold.co/150x60/f8f9fa/75848F?text=Company+5" class="img-fluid opacity-75" alt="Logo Patrocinador 5">
-				</div>
-				<!-- Logo 6 -->
-				<div class="col-auto flex-shrink-0 text-center" style="width: 200px;">
-					<img src="https://placehold.co/150x60/f8f9fa/75848F?text=Company+6" class="img-fluid opacity-75" alt="Logo Patrocinador 6">
-				</div>
-				<!-- Logo 7 -->
-				<div class="col-auto flex-shrink-0 text-center" style="width: 200px;">
-					<img src="https://placehold.co/150x60/f8f9fa/75848F?text=Company+7" class="img-fluid opacity-75" alt="Logo Patrocinador 7">
-				</div>
-				<!-- Logo 8 -->
-				<div class="col-auto flex-shrink-0 text-center" style="width: 200px;">
-					<img src="https://placehold.co/150x60/f8f9fa/75848F?text=Company+8" class="img-fluid opacity-75" alt="Logo Patrocinador 8">
-				</div>
-				<!-- Logo 9 -->
-				<div class="col-auto flex-shrink-0 text-center" style="width: 200px;">
-					<img src="https://placehold.co/150x60/f8f9fa/75848F?text=Company+9" class="img-fluid opacity-75" alt="Logo Patrocinador 9">
-				</div>
-				<!-- Logo 10 -->
-				<div class="col-auto flex-shrink-0 text-center" style="width: 200px;">
-					<img src="https://placehold.co/150x60/f8f9fa/75848F?text=Company+10" class="img-fluid opacity-75" alt="Logo Patrocinador 10">
-				</div>
-				<!-- Logo 11 -->
-				<div class="col-auto flex-shrink-0 text-center" style="width: 200px;">
-					<img src="https://placehold.co/150x60/f8f9fa/75848F?text=Company+11" class="img-fluid opacity-75" alt="Logo Patrocinador 11">
-				</div>
-				<!-- Logo 12 -->
-				<div class="col-auto flex-shrink-0 text-center" style="width: 200px;">
-					<img src="https://placehold.co/150x60/f8f9fa/75848F?text=Company+12" class="img-fluid opacity-75" alt="Logo Patrocinador 12">
-				</div>
-			</div>
-		</div>
-	</section>
+	<!-- PATROCINADORES -->
+	<?php include_once ('templates/patrocinadores.php'); ?>
 
 	<!-- SECCIÓN: COMUNICADOS DE PRENSA -->
 	<section id="comunicados-prensa" class="container-fluid bg-primary py-5">
